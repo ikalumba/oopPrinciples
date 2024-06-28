89a2587fe128a6db12eaeb51e634bab5369229f8
Benjamin Kiruube Maranatha <benjaminkiruube@gmail.com>
2024-06-28 07:09:26 +0300

    add folders

--- principlesJavaScript/inheritance.js
+++ principlesJavaScript/inheritance.js
@@ -1,36 +1,3 @@
-//Abstraction
-
-//Encapsulation
-class Employee {
-
-    setEmpDetails(name, id, phoneNo) {
-        this.name = name;
-        this.id = id;
-        this.phoneNo = phoneNo;
-    }
-
-    getEmpName() {
-        return this.name;
-    }
-    
-    getEmpId() {
-        return this.id;
-    }
-    
-    getEmpPhoneNo() {
-        return this.phoneNo;
-    }
-}
-
-let emp1 = new Employee();
-emp1.setEmpDetails('Marvin', 1, '0781304333');
-console.log(emp1.getEmpName()); // Output: Marvin
-console.log(emp1.getEmpId()); // Output: 1
-console.log(emp1.getEmpPhoneNo()); // Output: 0781304333
-
-
-//Polymorphism
-
 //Inheritance = allows a new class to inherit properties and methods
 //                         from an existing class. Helps with code reusability
 
